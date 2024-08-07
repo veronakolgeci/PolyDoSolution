ff47addbc9d70e9c76eb0bdea06108fbb5f457a9
verona kolgeci <verona.kolgeci@asseco-see.com>
2024-08-08 00:38:07 +0200

    add database connection string

--- PolyDo/Program.cs
+++ PolyDo/Program.cs
@@ -1,9 +1,16 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PolyDo.Application.DTOs;
+using PolyDo.Infrastructure.Data;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
