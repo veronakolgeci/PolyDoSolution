624855307364131f886efb643ce46c3910067a72
verona kolgeci <verona.kolgeci@asseco-see.com>
2024-08-08 11:54:59 +0200

    add views for controllers

--- PolyDo.Application/DTOs/TaskListDto.cs
+++ PolyDo.Application/DTOs/TaskListDto.cs
@@ -4,5 +4,6 @@ namespace PolyDo.Application.DTOs {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public List<ParentTaskDto> Tasks { get; set; }
     }
 }
